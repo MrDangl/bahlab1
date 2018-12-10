f7273d74db75f87d3f018f61eaa8535c2695b6e8
Gleb Danilov <glebdangl@gmail.com>
2018-12-10 21:20:11 +0800

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+    static  int  n =5;
+    Buffer a =new Buffer(n);
+    public static void main(String[] args)
+    {
+
+	// write your code here
+    }
+}
